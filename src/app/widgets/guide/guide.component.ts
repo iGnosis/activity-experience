a4f808b09e93e049df8133d42c415e3bb0e7179f
Aman Gautam <aman@pointmotioncontrol.com>
2022-05-05 15:16:57 +0530

    feat: 🎸 integrated timer with the activity
    ✅ Closes: SH-205

--- src/app/widgets/guide/guide.component.ts
+++ src/app/widgets/guide/guide.component.ts
@@ -24,92 +24,100 @@ export class GuideComponent implements AfterViewInit {
   @ViewChild('messageBottom') messageBottom!: ElementRef;
   @ViewChild('timer') timer!: ElementRef;
   avatarPosition = '';
-
+  clearTimeOut: any
+  
   constructor(private store: Store<{ guide: GuideState }>, private guideService: GuideService) {}
-
+  
   ngAfterViewInit(): void {
     this.store
-      .select((state) => state.guide.avatar)
-      .subscribe((avatar: GuideAvatarDTO | undefined) => {
-        if (avatar) {
-          this.handleAvatarUpdate(avatar);
-        } else {
-          this.handleHideAvatar();
-        }
-      });
-
+    .select((state) => state.guide.avatar)
+    .subscribe((avatar: GuideAvatarDTO | undefined) => {
+      if (avatar) {
+        this.handleAvatarUpdate(avatar);
+      } else {
+        this.handleHideAvatar();
+      }
+    });
+    
     this.store
-      .select((state) => state.guide.message)
-      .subscribe((message: GuideMessageDTO | undefined) => {
-        if (message) {
-          this.handleSendMessage(message);
-        } else {
-          this.handleHideMessage();
-        }
-      });
-
+    .select((state) => state.guide.message)
+    .subscribe((message: GuideMessageDTO | undefined) => {
+      if (message) {
+        this.handleSendMessage(message);
+      } else {
+        this.handleHideMessage();
+      }
+    });
+    
     this.store
-      .select((state) => state.guide.spotlight)
-      .subscribe((spotlight: GuideSpotlightDTO | undefined) => {
-        if (spotlight) {
-          this.handleSpotlight(spotlight);
-        } else {
-          this.handleHideSpotlight();
-        }
-      });
-
+    .select((state) => state.guide.spotlight)
+    .subscribe((spotlight: GuideSpotlightDTO | undefined) => {
+      if (spotlight) {
+        this.handleSpotlight(spotlight);
+      } else {
+        this.handleHideSpotlight();
+      }
+    });
+    
     this.store
-      .select((state) => state.guide.prompt)
-      .subscribe((prompt: GuidePromptDTO | undefined) => {
-        if (prompt) {
-          this.handlePrompt(prompt);
-        } else {
-          this.handleHidePrompt();
-        }
-      });
-
+    .select((state) => state.guide.prompt)
+    .subscribe((prompt: GuidePromptDTO | undefined) => {
+      if (prompt) {
+        this.handlePrompt(prompt);
+      } else {
+        this.handleHidePrompt();
+      }
+    });
+    
     this.store
-      .select((state) => state.guide.video)
-      .subscribe((video) => {
-        console.log('video', video);
-
-        if (video) {
-          this.state.video = video;
-        } else {
-          this.state.video = undefined;
-        }
-      });
-
+    .select((state) => state.guide.video)
+    .subscribe((video) => {
+      console.log('video', video);
+      
+      if (video) {
+        this.state.video = video;
+      } else {
+        this.state.video = undefined;
+      }
+    });
+    
     this.store
-      .select((state) => state.guide.timer)
-      .subscribe((timer) => {
-        if (timer) {
-          this.handleStartTimer(timer)
-        } else {
-          this.handleHideTimer()
-        }
-      })
+    .select((state) => state.guide.timer)
+    .subscribe((timer) => {
+      if (timer) {
+        this.handleStartTimer(timer)
+      } else {
+        this.handleHideTimer()
+      }
+    })
   }
-
+  
   handleStartTimer(timer: GuideTimerDTO) {
     // Most beautiful piece of code (shit) 
-    this.state.timer = timer
-    this.timer.nativeElement.style.width = '0vw'
+    this.state.timer = undefined
     setTimeout(() => {
-      this.timer.nativeElement.style.transitionDuration = timer.timeout +'ms'
+      this.state.timer = timer
       setTimeout(() => {
-        this.timer.nativeElement.style.width = '100vw'
+        this.timer.nativeElement.style.width = '0vw'
+        clearTimeout(this.clearTimeOut)
         setTimeout(() => {
-          this.state.timer = undefined
-        }, timer.timeout + 200);
-      });
-    }, 100)
+          this.timer.nativeElement.style.transitionDuration = timer.timeout +'ms'
+          setTimeout(() => {
+            this.timer.nativeElement.style.width = '100vw'
+            this.clearTimeOut = setTimeout(() => {
+              this.state.timer = undefined
+            }, timer.timeout + 200);
+          });
+        }, 100)
+      })
+    });
+    
   }
-
+  
   handleHideTimer() {
     this.state.timer = undefined
   }
-
+  
   handleSendMessage(newMessage: GuideMessageDTO | undefined) {
     this.state.message = newMessage;
     console.log(newMessage);
@@ -119,68 +127,69 @@ export class GuideComponent implements AfterViewInit {
       if (this.state.message.position != newMessage?.position) {
         // do some animation thingy
       }
-
+      
       if (this.state.message.text !== newMessage?.text) {
         this.state.message.text = newMessage?.text;
       }
     } else {
       this.state.message = newMessage;
     }
-
+    
     setTimeout(() => {
       // once the new message box is in position
       this.handleAvatarImagePosition();
     });
   }
-
+  
   // avatar image, expression or location can change
   handleAvatarUpdate(newAvatar: GuideAvatarDTO | undefined) {
     this.state.avatar = Object.assign({}, newAvatar);
     this.handleAvatarImagePosition();
   }
-
+  
   handleAvatarImagePosition() {
     if (!this.avatar || !this.avatar.nativeElement) return;
-
+    
     const result = this.guideService.getAvatarPosition(
       this.state.avatar,
       this.avatar?.nativeElement,
       this.messageCenter?.nativeElement,
       this.messageBottom?.nativeElement,
-    );
-    setTimeout(() => {
-      this.avatar.nativeElement.style.top = result.top;
-      this.avatar.nativeElement.style.left = result.left;
-    });
-  }
-
-  handleSpotlight(spotlight: GuideSpotlightDTO) {
-    this.state.spotlight = Object.assign({}, spotlight);
-  }
-
-  handlePrompt(prompt: GuidePromptDTO) {
-    this.state.prompt = Object.assign({}, prompt);
-    this.state.prompt.className +=
+      );
+      setTimeout(() => {
+        this.avatar.nativeElement.style.top = result.top;
+        this.avatar.nativeElement.style.left = result.left;
+      });
+    }
+    
+    handleSpotlight(spotlight: GuideSpotlightDTO) {
+      this.state.spotlight = Object.assign({}, spotlight);
+    }
+    
+    handlePrompt(prompt: GuidePromptDTO) {
+      this.state.prompt = Object.assign({}, prompt);
+      this.state.prompt.className +=
       ' ' + this.guideService.getPromptClassNames(this.state.prompt.position);
+    }
+    
+    handleHideAvatar() {
+      console.log('hide avatar');
+      this.state.avatar = undefined;
+    }
+    
+    handleHideMessage() {
+      this.state.message = undefined;
+    }
+    
+    handleHideSpotlight() {
+      this.state.spotlight = undefined;
+    }
+    
+    handleHidePrompt() {
+      console.log(this.state);
+      
+      this.state.prompt = undefined;
+      console.log(this.state);
+    }
   }
-
-  handleHideAvatar() {
-    console.log('hide avatar');
-    this.state.avatar = undefined;
-  }
-
-  handleHideMessage() {
-    this.state.message = undefined;
-  }
-
-  handleHideSpotlight() {
-    this.state.spotlight = undefined;
-  }
-
-  handleHidePrompt() {
-    console.log(this.state);
-
-    this.state.prompt = undefined;
-    console.log(this.state);
-  }
-}
+  
