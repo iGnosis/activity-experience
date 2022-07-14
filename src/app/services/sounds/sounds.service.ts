b7d15386f99f9c98603fecea0ab1b728b74a13fb
Mohan Vaddi <myselfgeek01@gmail.com>
2022-07-14 18:39:36 +0530

    fix: reduced instruction sound volume to 15%

--- src/app/services/sounds/sounds.service.ts
+++ src/app/services/sounds/sounds.service.ts
@@ -482,31 +482,31 @@ export class SoundsService {
     switch (genre) {
       case 'Classical':
         if (!this.classicalInstructionsSound.playing()) {
-          this.classicalInstructionsSound.volume(0.2);
+          this.classicalInstructionsSound.volume(0.15);
           this.classicalInstructionsSound.play();
         }
         break;
       case 'Jazz':
         if (!this.jazzInstructionsSound.playing()) {
-          this.jazzInstructionsSound.volume(0.2);
+          this.jazzInstructionsSound.volume(0.15);
           this.jazzInstructionsSound.play();
         }
         break;
       case 'Rock':
         if (!this.rockInstructionsSound.playing()) {
-          this.rockInstructionsSound.volume(0.2);
+          this.rockInstructionsSound.volume(0.15);
           this.rockInstructionsSound.play();
         }
         break;
       case 'Dance':
         if (!this.danceInstructionsSound.playing()) {
-          this.danceInstructionsSound.volume(0.2);
+          this.danceInstructionsSound.volume(0.15);
           this.danceInstructionsSound.play();
         }
         break;
       case 'Surprise Me!':
         if (!this.surpriseInstructionsSound.playing()) {
-          this.surpriseInstructionsSound.volume(0.2);
+          this.surpriseInstructionsSound.volume(0.15);
           this.surpriseInstructionsSound.play();
         }
         break;
