93a0e1c891ab1fa2ce904b0dd69d0349aab3961b
Deep Bhatt <dbhatt@pointmotion.us>
2022-08-08 14:02:35 +0530

    fix: 🐛 add prod env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,35 @@
-export const environment = {
-  production: true,
+import { Environment } from 'src/app/types/pointmotion';
+
+export const environment: Environment = {
   stageName: 'prod',
+  production: true,
+  speedUpSession: false,
+  analytics: {
+    calibration: true,
+  },
+  googleAnalyticsTrackingID: 'G-MTGG72G6ND',
+  endpoint: 'https://api.prod.pointmotioncontrol.com/v1/graphql',
+  apiEndpoint: 'https://services.prod.pointmotioncontrol.com',
+  postSessionRedirectEndpoint: 'https://provider.prod.pointmotioncontrol.com',
+  order: ['sit_stand_achieve', 'beat_boxer'],
+  settings: {
+    sit_stand_achieve: {
+      configuration: {
+        minCorrectReps: 10,
+        speed: 5000,
+      },
+    },
+    beat_boxer: {
+      configuration: {
+        minCorrectReps: 10,
+        speed: 5000,
+      },
+    },
+    sound_slicer: {
+      configuration: {
+        minCorrectReps: 10,
+        speed: 5000,
+      },
+    },
+  },
 };
