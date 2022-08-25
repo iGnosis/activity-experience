ca73049df3793212240a045a848bda9064d2029f
vigneshrajj <vigneshhrajj@gmail.com>
2022-08-25 15:24:11 +0530

    feat: create web socket connection for pose tracking

--- src/app/pose-tracker.worker.ts
+++ src/app/pose-tracker.worker.ts
@@ -1,14 +1,17 @@
 /// <reference lib="webworker" />
 
+import { environment } from 'src/environments/environment';
+
 interface PoseTracker {
   t: number;
   g: string;
   u: string;
   p: any[];
 }
-
+// const socket = new WebSocket(environment.apiEndpoint.replace('https://', 'wss://'));
+const socket = new WebSocket(environment.apiEndpoint);
 addEventListener('message', ({ data }: any) => {
   const { poseLandmarks: p, timestamp: t, gameId: g, userId: u } = data;
   if (p && p.filter((landmark: any) => landmark.visibility < 0.7).length > 0) return;
-  postMessage({ t, g, u, p });
+  socket.send(JSON.stringify({ t, g, u, p }));
 });
