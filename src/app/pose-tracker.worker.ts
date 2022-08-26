8d88badf6d6d6a920af1face5b2c51b8b0183d63
Deep Bhatt <dbhatt@pointmotion.us>
2022-08-26 13:34:54 +0530

    fix: 🐛 only consider key body points for saving pose data

--- src/app/pose-tracker.worker.ts
+++ src/app/pose-tracker.worker.ts
@@ -9,12 +9,19 @@ const poseTrackerFn = () => {
   return ({ data }: any) => {
     switch (data.type) {
       case 'connect':
-        endpoint = data;
+        endpoint = data.websocketEndpoint;
+        console.log('connecting to websocket endpoint!', endpoint);
         socket = io(endpoint);
         break;
       case 'update-pose':
         const { poseLandmarks: p, timestamp: t, gameId: g, userId: u } = data;
-        if (p && p.filter((landmark: any) => landmark.visibility < 0.7).length > 0) return;
+        const points = [12, 11, 24, 23, 26, 25];
+        const keyBodyPoints = points.map((point) => p[point]);
+        if (
+          keyBodyPoints &&
+          keyBodyPoints.filter((landmark: any) => landmark.visibility < 0.7).length > 0
+        )
+          return;
         if (socket) socket.send({ t, g, u, p });
         break;
       case 'game-end':
