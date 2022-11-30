2402531a9ea81c726047fdce5d7587b4502ade81
vigneshrajj <vigneshhrajj@gmail.com>
2022-11-30 11:27:01 +0530

    test: 💍 fix failing tests

--- src/app/services/theme/theme.service.spec.ts
+++ src/app/services/theme/theme.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ThemeService } from './theme.service';
+import { provideMockStore } from '@ngrx/store/testing';
 
 describe('ThemeService', () => {
   let service: ThemeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideMockStore({})],
+    });
     service = TestBed.inject(ThemeService);
   });
 
