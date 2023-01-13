63c0ac29e7c97d0be2190f02887f8980cfa25cfa
vigneshrajj <vigneshhrajj@gmail.com>
2023-01-13 14:56:30 +0530

    test: 💍 fix failing test

--- src/app/services/qa/qa.service.spec.ts
+++ src/app/services/qa/qa.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { QaService } from './qa.service';
 
@@ -6,7 +8,10 @@ describe('QaService', () => {
   let service: QaService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [provideMockStore({})],
+    });
     service = TestBed.inject(QaService);
   });
 
