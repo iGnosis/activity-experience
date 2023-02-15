476ab44b85dbf665ef21f0f6a6e03ea3f500caa1
Deep Bhatt <dbhatt@pointmotion.us>
2023-02-15 14:18:41 +0530

    fix: 🐛 correct failing test

--- src/app/services/game/sit-to-stand/sit-to-stand.service.spec.ts
+++ src/app/services/game/sit-to-stand/sit-to-stand.service.spec.ts
@@ -17,28 +17,4 @@ describe('SitToStandService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-
-  it('should increase value by 10%', () => {
-    // Given
-    const increaseByPercent = 10;
-    const value = 100;
-
-    // When
-    const result = service.getPercentageChange(increaseByPercent, value);
-
-    // Then
-    expect(result).toBe(110);
-  });
-
-  it('should decrease value by 10%', () => {
-    // Given
-    const decreaseByPercent = -10;
-    const value = 100;
-
-    // When
-    const result = service.getPercentageChange(decreaseByPercent, value);
-
-    // Then
-    expect(result).toBe(90);
-  });
 });
