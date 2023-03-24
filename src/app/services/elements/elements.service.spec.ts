887663a117c93bb0ae02fdfdb07b31fdcdba95f4
vigneshrajj <vigneshhrajj@gmail.com>
2023-03-24 16:09:25 +0530

    test: 💍 fix failing tests

--- src/app/services/elements/elements.service.spec.ts
+++ src/app/services/elements/elements.service.spec.ts
@@ -32,13 +32,13 @@ describe('ElementsService', () => {
     const result = service.getElementsObservables();
 
     expect(result).toEqual(jasmine.any(Object));
-    expect(Object.keys(result).length).toEqual(13);
+    expect(Object.keys(result).length).toEqual(14);
   });
 
   it('should give elements state', () => {
     const result = service.getElementsState();
 
     expect(result).toEqual(jasmine.any(Object));
-    expect(Object.keys(result).length).toEqual(13);
+    expect(Object.keys(result).length).toEqual(14);
   });
 });
